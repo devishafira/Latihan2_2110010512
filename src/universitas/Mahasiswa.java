2724cb85d4ea20d49e562da1d22ce2d32383e588
devishafira <dvshafira@gmail.com>
2023-06-16 20:52:17 +0800

    melengkapi class mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -2,6 +2,25 @@ package universitas;
 
 public class Mahasiswa {
     String nama;
-    int npm;
+    String npm;
     String prodi;
+    
+    public String getNama(){
+       return nama;  
+   }
+    public String getNpm(){
+       return npm;  
+   }
+    public String getProdi(){
+       return prodi;  
+   }
+    
+    public void setnama (String nama){
+       nama = nama;
+   }
+    
+    public void setnpm (String npm){
+       npm = npm;
+   }
+    
 }
