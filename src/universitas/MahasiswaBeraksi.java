14a1ce0c54f8ebf4f56580998b2cb712b6349d9e
devishafira <dvshafira@gmail.com>
2023-06-16 20:58:26 +0800

    menjalankan program

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -2,5 +2,17 @@ package universitas;
 
 
 public class MahasiswaBeraksi {
+    public static void main(String[] args){
+            
+    Mahasiswa mhs = new Mahasiswa("2110010512", " Devi shafira", " Teknik Informatika");
     
+    
+        System.out.println("Nama: "+mhs.getNama());
+        System.out.println("NPM: "+mhs.getNpm());
+        System.out.println("Prodi: "+mhs.getProdi());
+        
+        System.out.println(mhs.setDetail());
+
+    
+}
 }
